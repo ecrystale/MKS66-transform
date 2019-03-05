cca2d95e8396c63ff072f25596c87832ccf90904
ecrystale <elee19@stuy,edu>
2019-03-04 19:13:57 -0500

    update

--- parser.py
+++ parser.py
@@ -89,11 +89,12 @@ def parse_file(fname, points, transform, screen, color ):
     #return           
     #pass
 
- '''   
+'''   
 screen = new_screen()
 color = [ 0, 255, 0 ]
 edges = []
 transform = new_matrix()
 '''
+
 #parse_file( 'script2.txt', edges, transform, screen, color )
 #parse_file( 'script', edges, transform, screen, color )
